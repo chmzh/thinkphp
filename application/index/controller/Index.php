5860414c94668ab872fdea2bcdc59375df6f65ed
chenmingzhou <sd@dfh.com>
2017-02-06 15:43:19 +0800

    u

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -45,11 +45,14 @@ class Index
     {
         //$arr = Db::query("select * from user");
         //$a = Db::table("user")->where('id',1)->find();
-        $a = User::get(1);
+        //$a = User::get(1);
         //print_r($a);
-        echo $a->enabled;
+        //echo $a->enabled;
         //return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
-        //return view("index");
+  
+
+        // 渲染模板输出 并赋值模板变量
+        return view('index',['name'=>'thinkphp']);
     }
     
     public function hello()
